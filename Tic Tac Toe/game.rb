9539611b0a828fdf7e3721402a9c74a74ec99bbf
Yuval <yvw.bor@gmail.com>
2014-07-06 23:52:44 +0300

    Add turn() and run()

--- Tic Tac Toe/game.rb
+++ Tic Tac Toe/game.rb
@@ -8,10 +8,17 @@ module TicTacToe
     # start a game with given board size
     # and players hash:
     #  :sym => :player_type
-    def initialize(size, players={:x => :h, :o => :h})
-
+    def initialize(size, win_size, players={:x => :h, :o => :h})
       @board = Board.new(size)
       @moves_left = size ** 2
+      @game_over = false
+
+      if win_size > size
+        puts '======================================================'
+        puts '= Warning: winning size too large, board unwinnable! ='
+        puts '======================================================'
+      end
+      @win_size = win_size
 
       @players = Array.new
       players.each do |key, val|
@@ -25,14 +32,65 @@ module TicTacToe
       end
     end
 
+    def turn()
+      puts "#{@players.first.sym}'s turn!"
+      board.print_board
+      valid = false
+      # make player move
+      until valid do
+      # try and get a valid move format
+        begin
+          move = @players.first.make_move()
+        rescue ArgumentError => e
+          puts e
+          next
+        end
 
-    def is_tie?
-      @moves_left == 0
+        # check if move on board
+        move.map! { |m| m - 1 }
+        unless board.valid?(move[1], move[0])
+          puts "Invalid square, not on board"
+          next
+        end
+
+        # try and make the move on the board
+        begin
+          score = board.set_cell(move[1], move[0], @players.first.sym)
+        rescue ArgumentError => e
+          puts e
+          next
+        end
+
+        valid = true
+      end
+
+      # check if move is winning
+      if score == @win_size
+        board.print_board
+        puts "#{@players.first.sym} has won!"
+        @game_over = true
+      end
+
+      @moves_left -= 1
+      if @moves_left == 0
+        board.print_board
+        puts "It's a tie!!!"
+        @game_over = true
+      end
+
+      @players.push(@players.shift)
+    end
+
+    def run()
+      until @game_over
+        system "clear" or system "cls"
+        turn()
+      end
     end
   end
+
+
 end
 
-game = TicTacToe::Game.new(2, {x: :h, o: :h})
-p game.is_tie?
-p game.board
-p game.players
+game = TicTacToe::Game.new(2, 5, {X: :h, Y: :h})
+game.run()
--- Tic Tac Toe/player.rb
+++ Tic Tac Toe/player.rb
@@ -1,24 +1,25 @@
 module TicTacToe
-    class Player
-        def initialize(sym, name = 'Player')
-            @sym = sym
-            @name = name
-        end
+  class Player
+
+    attr_reader :sym, :name
+    def initialize(sym)
+      @sym = sym
+      @name = name
     end
-    
-    #Prompts user for a move of N N format.
-    #Raises 
-    class HumanPlayer < Player
-        def make_move()
-            print "Make your move (row column): "
-            move = gets.chomp
-            unless move =~ /\d+\s+\d+/
-                raise ArgumentError, "Invalid input format, expected \"NN NN\""
-            end
-            move = move.split(/\s+/).map { |m| m.to_i unless m.empty? }
-            return move.select {|m| m }
-            
-        end
+  end
+
+  #Prompts user for a move of N N format.
+  #Raises Invalid input format argument error
+  class HumanPlayer < Player
+    def make_move()
+      print "Make your move (row column): "
+      move = gets.chomp
+      unless move =~ /\d+\s+\d+/
+          raise ArgumentError, "Invalid input format, expected \"NN NN\""
+      end
+      move = move.split(/\s+/).map { |m| m.to_i unless m.empty? }
+      return move.select {|m| m }
+
     end
-        
+  end
 end
